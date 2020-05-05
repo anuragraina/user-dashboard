8edc86138b549812a14407515bbffcd843ff0165
Anurag Raina <rainakalhan90@gmail.com>
2020-05-06 03:12:23 +0530

    Set up basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Avatar, Menu } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+const { Header, Footer, Sider, Content } = Layout;
+const { SubMenu } = Menu;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Layout>
+				<Header style={{ padding: 10 }}>
+					<Avatar style={{ float: 'right' }} icon={<UserOutlined />} />
+				</Header>
+				<Layout>
+					<Sider>
+						<Menu theme="dark" mode="inline">
+							<SubMenu
+								key="sub2"
+								title={
+									<span>
+										<span>Profile</span>
+									</span>
+								}
+							>
+								<Menu.Item key="1">Settings</Menu.Item>
+								<Menu.Item key="2">Notifications</Menu.Item>
+							</SubMenu>
+						</Menu>
+					</Sider>
+					<Layout>
+						<Content>Content</Content>
+						<Footer>Footer</Footer>
+					</Layout>
+				</Layout>
+			</Layout>
+		</div>
+	);
 }
 
 export default App;
